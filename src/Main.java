c335213aaa093813c89b5fb4eee24720a54f1ed4
MRothardt <rothardt.marvin@gmail.com>
2026-05-22 11:04:49 +0200

    testtesttest

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ void main() {
     System.out.println("prog so geil jaa");
     System.out.println("maren berger beste frau");
     System.out.println("Maren Berger ich brauche Heilung");
+    System.out.println("prog so geil jaa");
+    System.out.println("maren berger beste frau");
+    System.out.println("Maren Berger ich brauche Heilung");
 }
 
