fb46db4d0395fe09682d89a1659a747e6e97bd4f
Tom Blumeyer <tom.blumeyer@std.dhsh.de>
2026-05-22 10:39:37 +0200

    kleine Test hallo ka

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.sql.SQLOutput;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    System.out.println("ka^");
+    System.out.println("k^");
 
     }
 
