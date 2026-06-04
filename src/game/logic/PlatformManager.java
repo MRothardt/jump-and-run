358e3ee2049c401196953c334e8d9ad1f1aebd1e
Tom Blumeyer <tom.blumeyer@std.dhsh.de>
2026-06-04 13:35:14 +0200

    Hintergrund und Minenarbeiter Design überarbeitet

--- src/game/logic/PlatformManager.java
+++ src/game/logic/PlatformManager.java
@@ -12,37 +12,23 @@ import java.util.ArrayList;
 public class PlatformManager {
 
     private ArrayList<Platform> plattformen;
+    private int naechsterPlattformIndex;
+
+    private final int[] xPositionen = {
+            150, 50, 230, 90, 260,
+            30, 210, 120, 270, 60,
+            240, 90, 180, 30, 250,
+            110, 280, 70, 220, 40
+    };
+    private final int startY = 540;
+    private final int abstand = 90;
+    private final int untereEntfernungsGrenze = 700;
 
     public PlatformManager() {
         plattformen = new ArrayList<>();
 
-        int[] xPositionen = {
-                150, 50, 230, 90, 260,
-                30, 210, 120, 270, 60,
-                240, 90, 180, 30, 250,
-                110, 280, 70, 220, 40
-        };
-
-        int startY = 540;
-        int abstand = 90;
-
-        for (int i = 0; i < 100; i++) {
-            int x = xPositionen[i % xPositionen.length];
-            int y = startY - i * abstand;
-
-            PlatformType typ;
-
-            if (i < 6) {
-                typ = PlatformType.NORMAL;
-            } else if (i % 18 == 0) {
-                typ = PlatformType.TRAMPOLIN;
-            } else if (i % 4 == 0 || i % 7 == 0) {
-                typ = PlatformType.BRUECHIG;
-            } else {
-                typ = PlatformType.NORMAL;
-            }
-
-            plattformen.add(new Platform(x, y, typ));
+        for (int i = 0; i < 12; i++) {
+            neuePlattformHinzufuegen(startY - i * abstand);
         }
     }
 
@@ -64,6 +50,42 @@ public class PlatformManager {
         for (Platform plattform : plattformen) {
             plattform.bewegeNachUnten(distanz);
         }
+
+        plattformen.removeIf(plattform -> plattform.getY() > untereEntfernungsGrenze);
+
+        while (oberstePlattformY() > -abstand) {
+            neuePlattformHinzufuegen(oberstePlattformY() - abstand);
+        }
+    }
+
+    private void neuePlattformHinzufuegen(int y) {
+        int x = xPositionen[naechsterPlattformIndex % xPositionen.length];
+        PlatformType typ;
+
+        if (naechsterPlattformIndex < 6) {
+            typ = PlatformType.NORMAL;
+        } else if (naechsterPlattformIndex % 18 == 0) {
+            typ = PlatformType.TRAMPOLIN;
+        } else if (naechsterPlattformIndex % 4 == 0 || naechsterPlattformIndex % 7 == 0) {
+            typ = PlatformType.BRUECHIG;
+        } else {
+            typ = PlatformType.NORMAL;
+        }
+
+        plattformen.add(new Platform(x, y, typ));
+        naechsterPlattformIndex++;
+    }
+
+    private int oberstePlattformY() {
+        int obersteY = Integer.MAX_VALUE;
+
+        for (Platform plattform : plattformen) {
+            if (plattform.getY() < obersteY) {
+                obersteY = plattform.getY();
+            }
+        }
+
+        return obersteY;
     }
 
     public ArrayList<Platform> getPlatforms() {
