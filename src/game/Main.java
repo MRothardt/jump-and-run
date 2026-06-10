ba6924ed3694fcf5fe1c0d251a32f116defcfa5f
Tom Blumeyer <tom.blumeyer@std.dhsh.de>
2026-06-10 14:50:13 +0200

    1.Grafiken wurden angepasst 2. Menü hinzugefügt 3. Lava Burst BETA hinzugefügt ( nicht fertig)

--- src/game/Main.java
+++ src/game/Main.java
@@ -6,7 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Spiel startet...");
-        System.out.println("Tom kommt...");
         GameFrame gameFrame = new GameFrame();
         gameFrame.setVisible(true);
     }
